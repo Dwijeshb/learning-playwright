f3be03387699cb123718169f56eb4432d60029d9
Dwijesh Badari <dwijeshbadari@gmail.com>
2026-02-16 20:20:43 +0530

    Initial commit: Add learning project with Playwright tests

---
+++ Capter_01_Basics/Verify_setup.js
@@ -0,0 +1,13 @@
+console.log(process.platform);
+
+console.log(process.platform);
+// MAC - DARWIN
+// WINDOWS - WIN32
+// LINUX - LINUX
+
+console.log(process.arch);
+// x64
+// arm64
+
+
+console.log("Node Version:", process.version);
