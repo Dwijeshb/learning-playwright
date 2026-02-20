d3c5b5c98ebbeb1ea8fbce6150709038a328c364
Dwijesh Badari <dwijeshbadari@gmail.com>
2026-02-20 08:59:49 +0530

    Add chapter_03/literal.js

---
+++ chapter_03/literal.js
@@ -0,0 +1,11 @@
+//template literal
+let name = 'John';
+let message = `Hello, ${name}!`;
+console.log(message); // Output: Hello, John!
+
+let a="dexter";
+let b="krall";
+console.log(`heloo maccha ${a},${b} how are you?`)
+
+let math=`multiplication of 40*20= $(40*200)`
+console.log(math)
