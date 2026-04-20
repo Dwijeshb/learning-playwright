5ff19dcb1b85e124c7626eb2be626095496af375
Dwijesh Badari <dwijeshbadari@gmail.com>
2026-04-20 23:41:21 +0530

    Add Playwright coding tests and project configuration

---
+++ type script/type_scriptcode.ts
@@ -0,0 +1,9 @@
+ let test: string = "Hello, TypeScript!";
+
+ function addNumbers(x: number, y: number): number {
+     return x + y;
+ }  
+
+ let output: number = addNumbers(5, 10); // This will return 15
+console .log(output); // This will output: 15
+ console.log(test); // This will output: Hello, TypeScript!
